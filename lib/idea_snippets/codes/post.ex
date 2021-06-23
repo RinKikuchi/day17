2467867fa2ce086b45edaf8c8f569f37219a14f3
root <root@DESKTOP-VCG2F7S.localdomain>
2021-06-23 12:00:12 +0900

    jikken

---
+++ lib/idea_snippets/codes/post.ex
@@ -0,0 +1,25 @@
+defmodule IdeaSnippets.Codes.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias IdeaSnippets.Comments.Comment
+
+  schema "posts" do
+    field :code, :string
+    field :description, :string
+    field :title, :string
+    field :user_id, :integer
+    field :published, Ecto.Enum, values: [:draft, :public, :limited, :private]
+
+    has_many(:comments, Comment)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :description, :code, :user_id, :published])
+    |> validate_required([:title, :description, :code, :published])
+  end
+end
